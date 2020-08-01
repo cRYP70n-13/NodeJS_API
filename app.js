2e293e8a847fe87a75e1fdb06988104db2b85179
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-08-01 20:31:38 +0100

    now we have the database and the server all setup and working as expected

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const connectDB = require('./config/db');
+
+// Load config file
+dotenv.config({path: './config/config.env'});
+
+connectDB();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log('this shit is up and running'));
