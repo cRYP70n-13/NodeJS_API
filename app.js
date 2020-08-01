642844650a34353b4485ba9fe4097ebbce038fee
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-08-01 21:56:13 +0100

    adding the passport strategy and the routes also setting up the middlewares for the route <filters> also setting up the express sessions

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const path = require('path');
+const passport = require('passport');
+const session  = require('express-session');
 
 const connectDB = require('./config/db');
 
 // Load config file
 dotenv.config({path: './config/config.env'});
+
+// Passport config
+require('./config/passport')(passport);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -16,6 +21,7 @@ connectDB();
 
 // Requiring my routes
 const indexRoute = require('./routes/index.routes.js');
+const authRoutes = require('routes/auth.routes');
 
 // Logging
 if (process.env.NODE_ENV === 'development') {
@@ -23,7 +29,18 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Static folder
-app.use(express.static(path.join(__dirname)))
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Sessions middleware
+app.use(session({
+	secret: 'cRYP70N',
+	resave: false,
+	saveUninitialized: false
+}));
+
+// the passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Handlebars
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
@@ -31,5 +48,6 @@ app.set('view engine', '.hbs');
 
 // Routes
 app.use('/', indexRoute);
+app.use('/auth', authRoutes);
 
 app.listen(PORT, () => console.log('this shit is up and running'));
